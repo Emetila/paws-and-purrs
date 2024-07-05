851f92c18185f1c990f8bb073ce1c2d46cafa7ef
Emetila <emetila83@gmail.com>
2024-07-05 23:55:06 +0100

    {

--- src/app/components/Hero/index.jsx
+++ src/app/components/Hero/index.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import style from './style.module.css';
 import {ReactComponent as Image} from '../../assets/images/hero.svg';
+import { CustomButton } from "../Button";
 
 export const HeroSection = () => {
     return (
         <div className={style.hero}>
             <Image className={style.image} />
             <div className={style.textbox}>
+                <div className={style.text}>
                 <h1>Spoil your furry friend with love</h1>
                 <p>Discover unique pet products that make a difference. We have Everything your pet needs for 
                     a happy life.</p>
+                </div>
+                <CustomButton onClick={()=>{}}>Shop Paws and Purrs</CustomButton>
             </div>
         </div>
     )
