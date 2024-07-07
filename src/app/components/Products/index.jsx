b47022d962bb74aaef6b7ff1d989066cfc697a8a
Emetila <emetila83@gmail.com>
2024-07-07 19:45:20 +0100

    Implemented product page

--- src/app/components/Products/index.jsx
+++ src/app/components/Products/index.jsx
@@ -1,17 +1,69 @@
 import React from "react";
 import { SideSection } from "../SideList";
 import style from './style.module.css';
+import { IoMdArrowDropdown } from "react-icons/io";
+import { Products } from "./product";
+import { CustomButton } from "../Button";
+import { MdKeyboardArrowLeft } from "react-icons/md";
+import { MdKeyboardArrowRight } from "react-icons/md";
 
 export const ProductSection = () => {
     return (
-        <>
+        <main className={style.container}>
             <aside className={style.container}>
                 <SideSection />
             </aside>
 
             <article>
-                <h1>Main area</h1>
+                <div className={style.headcotainer}>
+                    <p className={style.containertext}>1 - 15 of 1551 results</p>
+                    <div className={style.inputbox}>
+                        <p>Sort by</p>
+                        <div className={style.dropdown}>
+                            <p>Relevance</p>
+                            <IoMdArrowDropdown className={style.dropdownicon} />
+                        </div>
+                    </div>
+                </div>
+
+                <div className={style.products}>
+                    {Products.map((Products) => (
+                        <div className={style.productbox}>
+                            <div>
+                                <img className={style.productsimages} src={Products.Image} />
+
+                            </div>
+                            <div className={style.wrap}>
+                                <div className={style.name}>
+                                    <h5 className={style.heading}>{Products.title}</h5>
+                                    <p className={style.description}>{Products.description}</p>
+                                    <div className={style.ratingbox}>
+                                        <img className={style.rating} src={Products.rating} />
+                                        <p className={style.description2}>{Products.ratingnumber}</p>
+                                    </div>
+                                    <h2 className={style.amount}>{Products.amount}</h2>
+                                </div>
+                                <CustomButton className={style.button} onClick={() => { }} type="secondary">Add to cart</CustomButton>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+                <div className={style.naviation}>
+                    <MdKeyboardArrowLeft className={style.lefticon}/>
+                    <div className={style.number}>
+                        <p className={style.active}>1</p>
+                        <p>2</p>
+                        <p>3</p>
+                        <p>4</p>
+                        <p>5</p>
+                        <p>6</p>
+                        <p>...</p>
+                        <p>50</p>
+                    </div>
+                    <MdKeyboardArrowRight className={style.righticon} />
+                </div>
             </article>
-        </>
+        </main>
     )
 }
---
+++ src/app/components/Products/product.js
@@ -0,0 +1,153 @@
+import product1 from '../../assets/images/product1.svg';
+import product2 from '../../assets/images/product2.svg';
+import product3 from '../../assets/images/product3.svg';
+import product4 from '../../assets/images/product4.svg';
+import product5 from '../../assets/images/product5.svg';
+import product6 from '../../assets/images/product6.svg';
+import product7 from '../../assets/images/product7.svg';
+import product8 from '../../assets/images/product8.svg';
+import product9 from '../../assets/images/product9.svg';
+import product10 from '../../assets/images/product10.svg';
+import product11 from '../../assets/images/product11.svg';
+import product12 from '../../assets/images/product12.svg';
+import product13 from '../../assets/images/product13.svg';
+import product14 from '../../assets/images/product14.svg';
+import product15 from '../../assets/images/product15.svg';
+import favourite from '../../assets/icon/love.svg';
+import star from '../../assets/icon/stars.svg';
+
+export const Products = [{
+    Image: product1,
+    like: favourite,
+    title: "Cozy Cuddle Cave",
+    description: "The purr-fect hideaway for your furry friend",
+    rating: star,
+    ratingnumber: "290 ratings",
+    amount: "$39.99",
+},
+{
+    Image: product2,
+    like: favourite,
+    title: "Recycled Rope Toy",
+    description: "Eco-friendly toy for endless fun",
+    rating: star,
+    ratingnumber: "496 ratings",
+    amount: "$14.99",
+},
+{
+    Image: product3,
+    like: favourite,
+    title: "Feather Teaser Toy",
+    description: "Interactive toy for playful cats",
+    rating: star,
+    ratingnumber: "264 ratings",
+    amount: "$9.99",
+},
+{
+    Image: product4,
+    like: favourite,
+    title: "The Woof IQ Ball",
+    description: "Challenge Your Dog's Mind and Keep Them Active",
+    rating: star,
+    ratingnumber: "965 ratings",
+    amount: "$9.99",
+},
+{
+    Image: product5,
+    like: favourite,
+    title: "Cozy Cat Bed",
+    description: "Soft and warm bed for cat",
+    rating: star,
+    ratingnumber: "1,111 ratings",
+    amount: "$49.99",
+},
+{
+    Image: product6,
+    like: favourite,
+    title: "Customizable Pet Collar",
+    description: "Personalized collar with name tag",
+    rating: star,
+    ratingnumber: "450 ratings",
+    amount: "$24.99",
+},
+{
+    Image: product7,
+    like: favourite,
+    title: "Catnip Infused Mouse Toy",
+    description: "Irresistable toy for cats to chase",
+    rating: star,
+    ratingnumber: "1,087 ratings",
+    amount: "$7.99",
+},
+{
+    Image: product8,
+    like: favourite,
+    title: "Plush Squeaky Toy",
+    description: "Soft toy with a squeaker inside",
+    rating: star,
+    ratingnumber: "487 ratings",
+    amount: "$10.99",
+},
+{
+    Image: product9,
+    like: favourite,
+    title: "Luxurious Custom Pet Bed",
+    description: "Comfortable and stylish custom pet bed.",
+    rating: star,
+    ratingnumber: "3,000 ratings",
+    amount: "$59.99",
+},
+{
+    Image: product10,
+    like: favourite,
+    title: "Stainless Steel Feeding Bowl",
+    description: "Durable and easy to clean feeding bowl",
+    rating: star,
+    ratingnumber: "2,900 ratings",
+    amount: "$15.99",
+},
+{
+    Image: product11,
+    like: favourite,
+    title: "Cooling Mat",
+    description: "Keeps your pet cool during hot days",
+    rating: star,
+    ratingnumber: "4,060 ratings",
+    amount: "$29.99",
+},
+{
+    Image: product12,
+    like: favourite,
+    title: "Scratching Post",
+    description: "Sturdy post for cats to scratch and climb",
+    rating: star,
+    ratingnumber: "469 ratings",
+    amount: "$39.99",
+},
+{
+    Image: product13,
+    like: favourite,
+    title: "Harness",
+    description: "For dogs who love to explore",
+    rating: star,
+    ratingnumber: "271 ratings",
+    amount: "$16.99",
+},
+{
+    Image: product14,
+    like: favourite,
+    title: "Interactive Laser Toy",
+    description: "Lase pointer for hours of play",
+    rating: star,
+    ratingnumber: "105 ratings",
+    amount: "$11.99",
+},
+{
+    Image: product15,
+    like: favourite,
+    title: "Waterproof Dog Jacket",
+    description: "Keep your dog dry and comfortable",
+    rating: star,
+    ratingnumber: "69 ratings",
+    amount: "$34.99",
+}]
